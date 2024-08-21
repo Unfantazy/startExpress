36d43d86356bb021eb3717cad30ae073b0c48351
unfantazy <akuptsova@korusconsulting.ru>
2024-08-21 16:37:33 +0300

    fix

--- src/posts/middlewares/postValidators.ts
+++ src/posts/middlewares/postValidators.ts
@@ -24,7 +24,7 @@ export const contentValidator = body("content")
 export const blogIdValidator = body("blogId")
   .isString()
   .trim()
-  .isLength({ min: 1, max: 20 });
+  .isLength({ min: 1, max: 50 });
 
 export const postValidator = [
   adminMiddleware,
